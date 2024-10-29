d258d5e1ea3fa7501fb9454309a7358407bb0afa
Joe <3262202547@qq.com>
2024-10-29 23:53:48 +0800

    init

---
+++ vjudge/1_1_POJ1231/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+  // int a = 1223;
+  int b;
+  int c = 0;  
+  cout << c << endl;
+  //输出"Hello,world!"
+  return 0;
+}
